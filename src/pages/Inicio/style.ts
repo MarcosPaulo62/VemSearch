f9bb913499ad8863e809ebbdb98ff07e1f831b64
Annelise Silva da Rocha <annelisesr@gmail.com>
2023-08-18 01:54:21 -0300

    inicio e sobre prontos e com navegabilidade implementada

--- src/pages/Inicio/style.ts
+++ src/pages/Inicio/style.ts
@@ -13,4 +13,20 @@ align-items: center;
     height: 210px;
 }
 
+.title-inicio{
+    color: var(--off-white);
+    text-align: center;
+    margin-bottom: 50px;
+    width: 70%;
+}
 `
+
+export const InicioDivButton = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: flex-end;
+    width: 100%;
+    margin-top: -20px;
+`
+
