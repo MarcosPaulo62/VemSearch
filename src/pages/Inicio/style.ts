870a790a52afb438976743580ad59db6c09bf088
Annelise Silva da Rocha <annelisesr@gmail.com>
2023-08-18 00:48:20 -0300

    ajuste pag inicio

---
+++ src/pages/Inicio/style.ts
@@ -0,0 +1,16 @@
+import { styled } from "styled-components"
+
+
+export const InicioStyle = styled.div`
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+.imgLogo-inicio{
+    width: 220px;
+    height: 210px;
+}
+
+`
